b0b232ef5c8f5a01c0eb743983a6bb3f8d6f6ff5
liutao <liutaohangzhou@163.com>
2023-02-19 14:40:13 +0800

    :wrench: 修改配置

--- app/schedule/logTime.js
+++ app/schedule/logTime.js
@@ -6,8 +6,8 @@ class logTime extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      cron: '0 35 6,12,14,18,21 * * ?', // 每天的2点10分执行一次
-      type: 'all',// 指定所有的 worker 都需要执行
+      cron: '0 42 6,12,14,18,21 * * ?', // 每天的2点10分执行一次
+      type: 'worker',
     };
   }
   // subscribe 是真正定时任务执行时被运行的函数
